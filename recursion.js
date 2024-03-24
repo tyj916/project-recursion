5e931151dcf812e4f0bfc10629460481cf3924b4
Tan Yi Jun <tanyijun6@gmail.com>
2024-03-25 00:12:18 +0800

    Finish

--- recursion.js
+++ recursion.js
@@ -1,3 +1,34 @@
 #!/usr/bin/env node
 
-console.log(123);
+function fibs(num) {
+  const array = [];
+
+  let a = 0;
+  let b = 1;
+
+  for (let i = 0; i < num; i++) {
+    array.push(a);
+    let c = a + b;
+    a = b;
+    b = c;
+  }
+
+  return array;
+}
+
+function fib(num) {
+  return (num <= 1) ? num : fib(num - 1) + fib(num - 2);
+}
+
+function fibsRec(num) {
+  const array = [];
+  for (let i = 0; i < num; i++) {
+    array.push(fib(i));
+  }
+  return array;
+}
+
+
+
+console.log(fibs(8));
+console.log(fibsRec(8));
